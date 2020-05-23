6edd13e88f44c64eacb08c50e65b27852f7c918f
Dinozaur_G <karnah@mail.ru>
2020-05-23 14:36:15 +0700

    Аштшыр

--- Ball8/Form1.Designer.cs
+++ Ball8/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonForAnswer = new System.Windows.Forms.Button();
+            this.labelForAnswer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxForQuestion = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelForNum = new System.Windows.Forms.Label();
+            this.buttonForGuess = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonForAnswer);
+            this.panel1.Controls.Add(this.labelForAnswer);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBoxForQuestion);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(361, 411);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonForAnswer
+            // 
+            this.buttonForAnswer.Location = new System.Drawing.Point(137, 203);
+            this.buttonForAnswer.Name = "buttonForAnswer";
+            this.buttonForAnswer.Size = new System.Drawing.Size(75, 23);
+            this.buttonForAnswer.TabIndex = 3;
+            this.buttonForAnswer.Text = "Press!!!";
+            this.buttonForAnswer.UseVisualStyleBackColor = true;
+            this.buttonForAnswer.Click += new System.EventHandler(this.buttonForAnswer_Click);
+            // 
+            // labelForAnswer
+            // 
+            this.labelForAnswer.AutoSize = true;
+            this.labelForAnswer.Location = new System.Drawing.Point(153, 331);
+            this.labelForAnswer.Name = "labelForAnswer";
+            this.labelForAnswer.Size = new System.Drawing.Size(0, 17);
+            this.labelForAnswer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Задай вопрос машине";
+            // 
+            // textBoxForQuestion
+            // 
+            this.textBoxForQuestion.Location = new System.Drawing.Point(4, 119);
+            this.textBoxForQuestion.Name = "textBoxForQuestion";
+            this.textBoxForQuestion.Size = new System.Drawing.Size(354, 22);
+            this.textBoxForQuestion.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labelForNum);
+            this.panel2.Controls.Add(this.buttonForGuess);
+            this.panel2.Location = new System.Drawing.Point(427, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(361, 411);
+            this.panel2.TabIndex = 1;
+            // 
+            // labelForNum
+            // 
+            this.labelForNum.AutoSize = true;
+            this.labelForNum.Location = new System.Drawing.Point(165, 210);
+            this.labelForNum.Name = "labelForNum";
+            this.labelForNum.Size = new System.Drawing.Size(0, 17);
+            this.labelForNum.TabIndex = 1;
+            // 
+            // buttonForGuess
+            // 
+            this.buttonForGuess.Location = new System.Drawing.Point(99, 88);
+            this.buttonForGuess.Name = "buttonForGuess";
+            this.buttonForGuess.Size = new System.Drawing.Size(174, 87);
+            this.buttonForGuess.TabIndex = 0;
+            this.buttonForGuess.Text = "Загадай цифру от 1 до 10, и я её отгадаю";
+            this.buttonForGuess.UseVisualStyleBackColor = true;
+            this.buttonForGuess.Click += new System.EventHandler(this.buttonForGuess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonForAnswer;
+        private System.Windows.Forms.Label labelForAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxForQuestion;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelForNum;
+        private System.Windows.Forms.Button buttonForGuess;
     }
 }
 
--- Ball8/Form1.cs
+++ Ball8/Form1.cs
@@ -12,9 +12,41 @@ namespace Ball8
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();
+        double pForEightOne = 0.5;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void buttonForAnswer_Click(object sender, EventArgs e)
+        {
+            if (textBoxForQuestion.Text == "")
+            {
+                MessageBox.Show("А вопрос где?");
+            }
+            else
+            {
+                if(rnd.NextDouble() % 1 >= 0.5)
+                {
+                    labelForAnswer.Text = "Yes!";
+                }
+                else
+                {
+                    labelForAnswer.Text = "No!";
+                }
+            }
+        }
+        private void buttonForGuess_Click(object sender, EventArgs e)
+        {
+            double a = rnd.NextDouble() % 1;
+            for (double i = 0; i <= 1; i += 0.1)
+            {
+                if ((a - i) <= 0)
+                {
+                    labelForNum.Text = (i * 10).ToString();
+                    break;
+                }
+            }
         }
     }
 }
